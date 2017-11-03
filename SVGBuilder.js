0ed079bfc5695f5199da796fbf3958d7163f3a7f
Oleksandra Koshchii <koshcole@fel.cvut.cz>
2017-11-03 16:38:33 +0100

    rather visable visualisation of tree hierarchies working

--- SVGBuilder.js
+++ SVGBuilder.js
@@ -42,18 +42,24 @@ SVGBuilder.prototype.start = function(namespaces,   interfaces,   links, hierarc
 }
 
 SVGBuilder.prototype.drawRect
-    = function(nodes, width, height, fill)
+    = function(graph, nodes, width, height, fill, shiftX, classPadding)
 {
 
-    var classname = nodes[0].classname;
+    if(shiftX == undefined)
+        shiftX = 0;
 
-    return this.forceGraph.selectAll("rect ." + classname)
+    if(classPadding == undefined)
+        classPadding = '';
+
+    var classname = nodes[0].classname + classPadding;
+
+    return graph.selectAll("rect ." + classname)
         .data(nodes)
         .enter()
         .append("svg:rect")
         .attr("class", classname)
         .attr("x", function(d){
-            return d.x;
+            return d.x + shiftX;
         })
         .attr("y",function(d){
             return d.y;
@@ -65,6 +71,59 @@ SVGBuilder.prototype.drawRect
 
 
 }
+
+SVGBuilder.prototype.drawLinks =
+    function(graph, data, shiftX, classPadding )
+    {
+
+        if(shiftX == undefined)
+            shiftX = 0;
+
+        if(classPadding == undefined)
+            classPadding = '';
+
+        return graph.selectAll(".line")
+            .data(data)
+            .enter()
+            .append("line")
+            .attr("x1", function(d) {
+                return d.source.x + shiftX })
+            .attr("y1", function(d) { return d.source.y })
+            .attr("x2", function(d) { return d.target.x + shiftX })
+            .attr("y2", function(d) { return d.target.y })
+            .attr("marker-end", "url(#end)")
+            .style("stroke", "rgb(6,120,155)");
+    }
+
+SVGBuilder.prototype.drawText =
+    function(graph, data, textFunction, shiftX, classPadding )
+    {
+
+        if(shiftX == undefined)
+            shiftX = 0;
+
+        if(classPadding == undefined)
+            classPadding = '';
+
+        return graph.selectAll("text .text" + classPadding)
+            .data(data)
+            .enter()
+            .append("svg:text")
+            .attr("class", "text")
+            .attr("x", function(d){
+                return d.x + 5 + shiftX;
+            })
+            .attr("y", function(d){
+                return d.y + svg.INTERFACE_HEIGHT/2 - 5;
+            })
+            .attr("width", 90)
+            .attr("height", this.INTERFACE_HEIGHT)
+            .attr("fill", "black")
+            .attr("font-family", "Ariel Black")
+            .attr("font-size", 12)
+            .text(textFunction);
+
+    }
 
 
 
@@ -110,50 +169,20 @@ SVGBuilder.prototype.drawForceSimulationGraph = function(){
 
 
     var namespaceNodes = this.drawRect(
-        this.namespaces, this.NAMESPACE_WIDTH, this.NAMESPACE_HEIGHT, this.NAMESPACE_FILL
+        this.forceGraph, this.namespaces, this.NAMESPACE_WIDTH, this.NAMESPACE_HEIGHT, this.NAMESPACE_FILL
     );
 
     var interfaceNodes = this.drawRect(
-        this.interfaces, this.INTERFACE_WIDTH, this.INTERFACE_HEIGHT, this.INTERFACE_FILL
+        this.forceGraph, this.interfaces, this.INTERFACE_WIDTH, this.INTERFACE_HEIGHT, this.INTERFACE_FILL
     );
 
-    var lines = this.forceGraph.selectAll(".line")
-        .data(this.links)
-        .enter()
-        .append("line")
-        .attr("x1", function(d) {
-            return d.source.x })
-        .attr("y1", function(d) { return d.source.y })
-        .attr("x2", function(d) { return d.target.x })
-        .attr("y2", function(d) { return d.target.y })
-        .attr("marker-end", "url(#end)")
-        .style("stroke", "rgb(6,120,155)");
+    var lines = this.drawLinks(this.forceGraph, this.links);
 
-
-
-
-    var titles = this.forceGraph.selectAll("text .text")
-        .data(this.interfaces)
-        .enter()
-        .append("svg:text")
-        .attr("class", "text")
-        .attr("x", function(d){
-            return d.x + 5;
-        })
-        .attr("y", function(d){
-            return d.y + svg.INTERFACE_HEIGHT/2 - 5;
-        })
-        .attr("width", 90)
-        .attr("height", this.INTERFACE_HEIGHT)
-        .attr("fill", "black")
-        .attr("font-family", "Ariel Black")
-        .attr("font-size", 12)
-        .text(function (d){
-            return d.json.id;
-        });
-
-
-
+    var titles = this.drawText(
+        this.forceGraph, this.interfaces,
+        function (d){
+        return d.json.id;
+    });
 
     var ticking = function() {
 
@@ -247,6 +276,15 @@ SVGBuilder.prototype.drawTreeGraph = function() {
     treeLayout.size([width , this.HEIGHT]);
     treeLayout(this.hierarchies[i]);
 
+    this.drawRect(
+        this.treeGraph,
+        this.hierarchies[i].descendants(),
+        this.INTERFACE_WIDTH, this.INTERFACE_HEIGHT,
+        this.INTERFACE_FILL,
+        width *i,
+        i);
+
+
     this.treeGraph
         .selectAll('circle.node' + i)
         .data(this.hierarchies[i].descendants())
@@ -262,25 +300,19 @@ SVGBuilder.prototype.drawTreeGraph = function() {
         .attr('r', this.RADIUS );
 
 // Links
-    this.treeGraph
-        .selectAll('line.link' + i)
-        .data(this.hierarchies[i].links())
-        .enter()
-        .append('line')
-        .classed('link' + i, true)
-        .attr('x1', function (d) {
-            return d.source.x + width*i;
-        })
-        .attr('y1', function (d) {
-            return d.source.y ;
-        })
-        .attr('x2', function (d) {
-            return d.target.x + width*i;
-        })
-        .attr('y2', function (d) {
-            return d.target.y ;
-        })
-        .style("stroke", "rgb(6,120,155)");
+
+this.drawLinks(this.treeGraph, this.hierarchies[i].links(), width*i, i );
+
+console.log(this.hierarchies[i].descendants());
+
+this.drawText(
+    this.treeGraph,
+    this.hierarchies[i].descendants(),
+    function(d){
+        return d.data.json.id;
+    },
+    width*i,
+    i)
 
 }
 
